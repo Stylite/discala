dbf05bf8c1daf46c14acac7e24bb556899b0d51c
Connor Scialdone <connor@connorjulian.org>
2017-09-04 00:26:37 -0600

    Add utilities for easier handling of Option and circe's Decoder.Result

--- TestBot.scala
+++ TestBot.scala
@@ -1,6 +1,9 @@
 package com.tripl3dogdare.discala
 package testbot
 
+import io.circe.literal._, io.circe.generic.auto._
+import data._
+
 object TestBot {
   def main(args:Array[String]) {
     val bot = new Discala().login(readFile(".auth"))
--- src/main/scala/com/tripl3dogdare/discala/package.scala
+++ src/main/scala/com/tripl3dogdare/discala/package.scala
@@ -2,6 +2,7 @@ package com.tripl3dogdare
 package object discala {
   import io.circe._, io.circe.parser._
   import io.circe.optics.JsonPath.{root => root_}
+  import io.circe.Decoder.Result
 
   def root = root_
   def jsonFrom(raw:String) = new DSJson(parse(raw).getOrElse(Json.Null))
@@ -14,19 +15,18 @@ package object discala {
     def apply[A](path:monocle.Optional[Json,A]):A = path.getOption(this.json).getOrElse(null.asInstanceOf[A])
   }
   
-  def readFile(path:String) = {
-    val source = scala.io.Source.fromFile(path)
-    try source.mkString finally source.close
-  }
-
-  implicit class NullOption[A <: AnyRef](a: A) {
-    def |?|[B >: A](b: => B) = if (a ne null) a else b
-  }
+  implicit class NullOption[A](opt:Option[A]) { def ? :A = opt.getOrElse(null.asInstanceOf[A]) }
+  implicit class CirceResult[_,A](eith:Either[_,A]) { def ? :A = eith.right.getOrElse(null.asInstanceOf[A]) }
 
   import java.util.{Timer, TimerTask}
   implicit class TimerOps(timer:Timer) {
     def setTimeout(f:() => Unit, time:Long) = timer.schedule(wrapTimerTask(f), time)
     def setInterval(f:() => Unit, time:Long) = timer.schedule(wrapTimerTask(f), time, time)
     def wrapTimerTask(f:() => Unit):TimerTask = new TimerTask { def run = f() }
+  }
+  
+  def readFile(path:String) = {
+    val source = scala.io.Source.fromFile(path)
+    try source.mkString finally source.close
   }
 }
